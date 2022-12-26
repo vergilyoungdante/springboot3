ab5f7fc2369bf79c49490bd4e5216ad69e21dd2e
vergil <yjb387759719@outlook.com>
2022-12-26 13:39:41 +0800

    初始化项目

---
+++ src/main/java/com/example/springboot3/Springboot3Application.java
@@ -0,0 +1,14 @@
+package com.example.springboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Springboot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3Application.class, args);
+    }
+
+}

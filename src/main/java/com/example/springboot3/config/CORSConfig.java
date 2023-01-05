5b654543e3332302acb00bcec18d1c18d8272e73
vergil <yjb387759719@outlook.com>
2023-01-05 17:17:35 +0800

    优雅关机

---
+++ src/main/java/com/example/springboot3/config/CORSConfig.java
@@ -0,0 +1,35 @@
+package com.example.springboot3.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @BelongsProject: springboot3
+ * @BelongsPackage: com.example.springboot3.config
+ * @Author: vergil young
+ * @CreateTime: 2023-01-05  11:07
+ * @Description: 跨域配置
+ */
+
+@Configuration(proxyBeanMethods = false)
+public class CORSConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        //是否允许发放cookie信息
+                        .allowCredentials(true)
+                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                        .maxAge(3600);
+            }
+
+        };
+    }
+}
